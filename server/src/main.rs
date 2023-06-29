5bbe7e4f68bf53964a057a5a2bfd10131f6b8b30
yohashinoio <shinodeveloper1@gmail.com>
2023-06-29 17:10:51 +0900

    Allowed to specify server port by PORT

--- server/src/main.rs
+++ server/src/main.rs
@@ -77,6 +77,21 @@ impl AppState {
     }
 }
 
+fn get_expose_port() -> u16 {
+    const P: u16 = 8080;
+
+    match std::env::var("PORT") {
+        Ok(port) => match port.parse::<u16>() {
+            Ok(port) => port,
+            Err(_) => {
+                tracing::error!("Invalid expose port specified: {}", port);
+                P
+            }
+        },
+        Err(_) => P,
+    }
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
@@ -101,7 +116,7 @@ async fn main() {
                 .on_response(DefaultOnResponse::new().level(Level::INFO)),
         );
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], get_expose_port()));
 
     tracing::info!("Listening on {}", addr);
 
