4a02e154e0727d7cb297604395c845630bbebcf7
yohashinoio <shinodeveloper1@gmail.com>
2023-06-29 22:26:37 +0900

    Update client
    NEXT_PUBLIC_SRV_HOST to NEXT_PUBLIC_SRV_HOST_PORT

--- client/web/app/_util/server.ts
+++ client/web/app/_util/server.ts
@@ -1,2 +1,2 @@
-export const server_origin_http = `http://${process.env.NEXT_PUBLIC_SRV_HOST}:8080`;
-export const server_origin_websocket = `ws://${process.env.NEXT_PUBLIC_SRV_HOST}:8080`;
+export const server_origin_http = `http://${process.env.NEXT_PUBLIC_SRV_HOST}`;
+export const server_origin_websocket = `ws://${process.env.NEXT_PUBLIC_SRV_HOST}`;
